55d8b0652d20c57e8b7f0c7b9147e4da671e7483
Samuu <samuelfilipevianadois@gmail.com>
2024-05-17 15:46:18 -0300

    teste

--- client/src/pages/Auth/Login/Login.jsx
+++ client/src/pages/Auth/Login/Login.jsx
@@ -40,6 +40,7 @@ export default function Login(props) {
     return (
 
         <div className='bg-gray-100 w-full h-screen m-0 flex items-center'>
+            <ToastContainer />
             <div className="flex sm:mx-auto sm:w-full sm:max-w-xl flex-1 flex-col justify-center px-0 py-12 lg:px-16 bg-white rounded-xl">
                 <div className="sm:mx-auto ">
                     <img
@@ -90,7 +91,7 @@ export default function Login(props) {
                                     id="check" />
                                 <span
                                     className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor"
+                                    <svg xmlns="http://www.w3.org/2000/svg" cl="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor"
                                         stroke="currentColor" strokeWidth="1">
                                         <path fillRule="evenodd"
                                             d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"

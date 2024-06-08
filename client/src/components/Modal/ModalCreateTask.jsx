33b59634422c9cb13a88dd9abfa5d084880444f7
Sofia <sofia@MacBook-Pro-Sofia.local>
2024-06-08 18:15:37 -0300

    container create task ainda no modal

--- client/src/components/Modal/ModalCreateTask.jsx
+++ client/src/components/Modal/ModalCreateTask.jsx
@@ -1,36 +1,35 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Switch from '../Switch';
-import { select } from 'redux-saga/effects';
 
 export default function ModalCreateTask({ isOpen, setOpenModal }) {
+    const nomeTarefaRef = useRef(null);
+    const categoriaSelectRef = useRef(null);
+    const urgenciaSelectRef = useRef(null);
 
     function criarTarefa() {
-        var nomeTarefa = document.getElementById('nome-tarefa').value;
-        var selectCategoria = document.getElementById('categoria-select').value; // Alterado para o ID correto
-        var selectUrgencia = document.getElementById('urgencia-select').value; 
-        console.log(nomeTarefa); 
+        const nomeTarefa = nomeTarefaRef.current.value;
+        const selectCategoria = categoriaSelectRef.current.value;
+        const selectUrgencia = urgenciaSelectRef.current.value;
+        console.log(nomeTarefa);
         console.log(selectCategoria);
         console.log(selectUrgencia);
-    
-        var newTask = document.getElementById('new-task')
+
+        const newTask = document.getElementById('new-task');
         newTask.innerHTML += `
-            <div>
-               <input type='checkbox' />
-               <div>
-                    <div className='flex'>
+            <div class='flex items-center gap-4 bg-gray-200 px-4 py-2 rounded-md'>
+                <input type='checkbox' />
+                <div>
+                    <div class='flex'>
                         <span>${selectCategoria}</span>
                         <div></div>
                     </div>
-
                     <div> 
                         <span>${nomeTarefa}</span>
                     </div>
-               </div>
-                
+                </div>
             </div>
-        `
+        `;
     }
-    
 
     if (!isOpen) {
         return null;
@@ -44,46 +43,54 @@ export default function ModalCreateTask({ isOpen, setOpenModal }) {
                         <path fill="#820ad1" d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
                     </svg>
                 </button>
-
-                <input className="bg-gray-200 w-full px-4 rounded-md py-2 text-lg font-medium" placeholder="Tarefa" type="text" id='nome-tarefa' />
-
+                <input
+                    className="bg-gray-200 w-full px-4 rounded-md py-2 text-lg font-medium"
+                    placeholder="Tarefa"
+                    type="text"
+                    ref={nomeTarefaRef}
+                />
                 <div className="flex gap-8">
-                    <select className="bg-gray-200 w-full px-4 rounded-md py-2 text-lg font-base text-gray-400">
-                        <option className='text-gray-400' value="" selected disabled>Categoria</option>
+                    <select
+                        className="bg-gray-200 w-full px-4 rounded-md py-2 text-lg font-base text-gray-400"
+                        ref={categoriaSelectRef}
+                    >
+                        <option className="text-gray-400" value="" selected disabled>
+                            Categoria
+                        </option>
                         <option value="Escola">Escola</option>
                         <option value="Trabalho">Trabalho</option>
                     </select>
-
-                    <select id="urgencia-select" className="bg-gray-200 w-full px-4 rounded-md py-2 text-lg font-base text-gray-400">
-                        <option selected disabled value="">Urgência</option>
+                    <select
+                        id="urgencia-select"
+                        className="bg-gray-200 w-full px-4 rounded-md py-2 text-lg font-base text-gray-400"
+                        ref={urgenciaSelectRef}
+                    >
+                        <option selected disabled value="">
+                            Urgência
+                        </option>
                         <option value="Urgente">Urgente</option>
                         <option value="Moderado">Moderado</option>
                         <option value="Tranquilo">Tranquilo</option>
                     </select>
-
                 </div>
-
                 <div className="flex gap-8">
                     <div className="bg-gray-200 w-full px-4 rounded-md text-lg flex items-center justify-between">
-                        <span className='text-gray-400'>Dia</span>
+                        <span className="text-gray-400">Dia</span>
                         <Switch />
                     </div>
-
-                    <div className='flex gap-10'>
+                    <div className="flex gap-10">
                         <div className="bg-gray-200 w-full px-4 py-2 rounded-md text-lg text-center flex items-center justify-center">
-                            <input className='bg-gray-200 m-auto' type="time" />
+                            <input className="bg-gray-200 m-auto" type="time" />
                         </div>
                         <div className="bg-gray-200 w-full px-4 py-2 rounded-md text-lg text-center flex items-center justify-center">
-                            <input className='bg-gray-200 m-auto' type="date" />
+                            <input className="bg-gray-200 m-auto" type="date" />
                         </div>
                     </div>
                 </div>
-
-                <button onClick={criarTarefa} className="bg-blue-500 text-white px-4 py-2 rounded-md">Criar Tarefa</button>
-
-                <div id='new-task'>
-
-                </div>
+                <button onClick={criarTarefa} className="bg-blue-500 text-white px-4 py-2 rounded-md">
+                    Criar Tarefa
+                </button>
+                <div id="new-task" className="mt-4"></div>
             </div>
         </div>
     );

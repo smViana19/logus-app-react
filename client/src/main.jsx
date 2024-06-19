0081b318c9f7ea1663c2a0d057332b9b7c04e743
unknown <sofiapparreira@gmail.com>
2024-06-19 19:00:17 -0300

    messes passando com as setas

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
+import ContextWrapper from './context/ContextWrapper.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ContextWrapper>
     <App />
+
+    </ContextWrapper>
   </React.StrictMode>,
 )

05d4541c6871f07baf44c0fbc2660f3621c2fe75
unknown <sofiapparreira@gmail.com>
2024-06-21 19:25:00 -0300

    displaying events on individuals days

--- client/src/components/GoogleAgenda/Day.jsx
+++ client/src/components/GoogleAgenda/Day.jsx
@@ -1,14 +1,25 @@
-import React, { useContext } from 'react';
-import dayjs from 'dayjs'; 
+import React, { useContext, useEffect, useState } from 'react';
+import dayjs from 'dayjs';
 import GlobalContext from '../../context/GlobalContext';
 
+const labelColorMap = {
+    work: 'bg-blue-200',
+    personal: 'bg-green-200',
+    holiday: 'bg-red-200',
+};
+
 export default function Day({ day, rowIdx }) {
+    const [dayEvents, setDayEvents] = useState([]);
+    const { setDaySelected, setShowEventModal, savedEvents } = useContext(GlobalContext);
+
+    useEffect(() => {
+        const events = savedEvents.filter(evt => dayjs(evt.day).format('DD-MM-YY') === day.format('DD-MM-YY'));
+        setDayEvents(events);
+    }, [savedEvents, day]);
 
     function getCurrentDayClass() {
-        return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY") ? 'bg-blue-600 text-white rounded-full w-7' : '';
+        return day.format('DD-MM-YY') === dayjs().format('DD-MM-YY') ? 'bg-blue-600 text-white rounded-full w-7' : '';
     }
-
-    const { setDaySelected, setShowEventModal } = useContext(GlobalContext);
 
     const handleDayClick = () => {
         setDaySelected(day);
@@ -16,10 +27,10 @@ export default function Day({ day, rowIdx }) {
     };
 
     return (
-        <div className='border border-gray-200 flex flex-col'>
-            <div className='flex flex-col items-center'>
+        <div className="border border-gray-200 flex flex-col">
+            <div className="flex flex-col items-center">
                 {rowIdx === 0 && (
-                    <p className='text-sm mt-1'>
+                    <p className="text-sm mt-1">
                         {day.format('ddd').toUpperCase()}
                     </p>
                 )}
@@ -28,8 +39,14 @@ export default function Day({ day, rowIdx }) {
                 </p>
             </div>
 
-            <div className='flex-1 cursor-pointer' onClick={handleDayClick}>
-               
+            <div className="flex-1 cursor-pointer" onClick={handleDayClick}>
+                {dayEvents.map((evt, idx) => (
+                    <div
+                        key={idx}
+                        className={`bg-${evt.label}-200 p-1 mr-3 text-gray-600 text-sm rounded mb-1 truncate`}>
+                        {evt.title}
+                    </div>
+                ))}
             </div>
         </div>
     );
--- client/src/components/GoogleAgenda/EventModal.jsx
+++ client/src/components/GoogleAgenda/EventModal.jsx
@@ -47,6 +47,8 @@ export default function EventModal() {
 
     setShowEventModal(false);
   }
+
+
   return (
     <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center">
       <form className="bg-white rounded-lg shadow-2xl w-1/4">
@@ -71,7 +73,9 @@ export default function EventModal() {
             )}
             <button onClick={() => setShowEventModal(false)}>
               <span className="material-icons-outlined text-gray-400">
-                close
+                
+              <svg xmlns="http://www.w3.org/2000/svg" height="22" width="22" viewBox="0 0 512 512">
+                <path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"/></svg>
               </span>
             </button>
           </div>

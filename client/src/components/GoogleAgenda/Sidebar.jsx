b3ed46e93e472696cc9506ed23ea51b0a4780370
unknown <sofiapparreira@gmail.com>
2024-06-22 14:36:01 -0300

    começo da responsividade da agenda

--- client/src/components/GoogleAgenda/Sidebar.jsx
+++ client/src/components/GoogleAgenda/Sidebar.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import CreateEventButton from './CreateEventButton'
 import SmallCalendar from './SmallCalendar'
+import styled from 'styled-components'
 
 export default function Sidebar() {
   return (
-    <aside className='border p-5 w-64'>
+    <MenuLateral className='border p-5 w-64'>
         <CreateEventButton />
         <SmallCalendar /> 
-    </aside>
+    </MenuLateral>
   )
 }
+
+const MenuLateral = styled.aside`
+  @media (max-width: 768px) {
+    display: none;
+  }
+`

a6a7d11a9fcc1e0ceb73dc8ad4d1909e63ac41fa
Toddi <fernando.b.m2006@gmail.com>
2024-07-22 08:45:36 -0300

    padronização cabeçalho pt3

--- client/src/pages/Materias/MateriaPage.jsx
+++ client/src/pages/Materias/MateriaPage.jsx
@@ -97,6 +97,14 @@ const MateriaPage = () => {
                                     >
                                         Método Pomodoro
                                     </NavLink>
+                                    <NavLink href="#" to='/dashboard/notas'>Notas</NavLink>
+                                    <NavLink
+                                        // borderPage={currentRoute === '/agenda' ? '1px solid black' : 'none'}
+                                        href="#" to='/dashboard/perfil' >
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="14" width="12.25" viewBox="0 0 448 512">
+                                            <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/>
+                                        </svg>
+                                    </NavLink>
                                     <LogoutButton />
                                 </div>
                             </div>

7887e4fa9d231d2d069391c7a834ccb5d82adb2e
Toddi <fernando.b.m2006@gmail.com>
2024-07-24 15:40:38 -0300

    darkmode dashboard totalmente funcional pt2

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -43,11 +43,11 @@ export default function Dashboard() {
 
                     <div className="flex justify-around">
                         <div className="space-x-8 lg:flex">
-                            <NavLink to="/dashboard">Dashboard</NavLink>
-                            <NavLink to="/dashboard/postagens">Área de Postagens</NavLink>
-                            <NavLink to="/dashboard/agenda">Agenda</NavLink>
-                            <NavLink to="/dashboard/pomodoro">Método Pomodoro</NavLink>
-                            <NavLink to="/dashboard/notas">Notas</NavLink>
+                            <NavLink className={`text-lg ${darkMode ? 'text-black' : 'text-black'}`} to="/dashboard">Dashboard</NavLink>
+                            <NavLink className={`text-lg ${darkMode ? 'text-black' : 'text-black'}`} to="/dashboard/postagens">Área de Postagens</NavLink>
+                            <NavLink className={`text-lg ${darkMode ? 'text-black' : 'text-black'}`} to="/dashboard/agenda">Agenda</NavLink>
+                            <NavLink className={`text-lg ${darkMode ? 'text-black' : 'text-black'}`} to="/dashboard/pomodoro">Método Pomodoro</NavLink>
+                            <NavLink className={`text-lg ${darkMode ? 'text-black' : 'text-black'}`} to="/dashboard/notas">Notas</NavLink>
                         </div>
                     </div>
                     <label className="inline-flex items-center relative">
@@ -87,14 +87,16 @@ export default function Dashboard() {
                                 height="14"
                                 width="12.25"
                                 viewBox="0 0 448 512"
+                                className={`fill-gray-400 ${darkMode ? 'text-gray-200' : ''}`}
                             >
-                                <path className={`fill-gray-400 ${darkMode ? 'dark:fill-gray-200' : ''}`} d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
+                                <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
                             </svg>
                         </NavLink>
                         <LogoutButton />
                     </div>
                 </div>
             </nav>
+
 
             <UsuarioCard role={role} user={user} />
 

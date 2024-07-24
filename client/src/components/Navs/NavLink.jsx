cca0a450530ea8fa43c9834c1d35e3be03e3b735
sofia <sofiapparreira@gmail.com>
2024-07-24 12:13:19 -0300

    padronizei a nova nav em todas as páginas - menos adm

--- client/src/components/Navs/NavLink.jsx
+++ client/src/components/Navs/NavLink.jsx
@@ -1,17 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-export default function NavLink({ active = false, className = '', children, ...props }) {
+export default function NavLink({ to, className = '', children, ...props }) {
+    const location = useLocation();
+    const isActive = location.pathname === to;
+
     return (
         <Link
+            to={to}
             {...props}
             className={
-                'inline-flex items-center px-1 pt-1 leading-5 transition duration-150 ease-in-out focus:outline-none' +
-                (active
-                    ? 'border-indigo-400 text-gray-900 focus:border-indigo-700 '
-                    : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300 ') +
+                'inline-flex items-center px-2 leading-5 ease-in-out focus:outline-none text-gray-400 transition-all duration-150 ' +
+                (isActive
+                    ? 'border-b-2 border-purplePrimary text-purplePrimary '
+                    : 'border-b-2 border-transparent hover:text-gray-800 hover:border-gray-300 ') +
                 className
-
-
             }
         >
             {children}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -9,95 +9,92 @@ import CardCategoria from '../components/CardsContainers/CardCategoria';
 import LogoutButton from '../components/Botoes/LogoutBtn';
 import MenuMobile from '../components/Navs/MenuMobile';
 
-
 export default function Dashboard() {
+    const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+    const role = useSelector((state) => state.auth.user?.role);
+    const user = useSelector((state) => state.auth.user?.nome);
 
-    const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
-    const role = useSelector(state => state.auth.user?.role);
-    const user = useSelector(state => state.auth.user?.nome);
-
- //  if (!isLoggedIn) {
-  //    return <Navigate to="/login" replace />;
+    //  if (!isLoggedIn) {
+    //    return <Navigate to="/login" replace />;
     //}
+
     return (
         <>
             <div className="min-h-screen bg-gray-50">
-                <nav className="bg-white border-b border-gray-100">
-                <MenuMobile/>
-                    <div className="flex justify-between h-16 px-32">
-
-
+                <nav className="bg-white border-b border-gray-50 shadow-md shadow-gray-50">
+                    <MenuMobile />
+                    <div className="flex justify-between py-2 px-32">
                         <div className="flex items-center">
-                                    <Link to="/">
-                                        <Logo className="block h-9 w-auto fill-current text-gray-800" />
-                                    </Link>
-                                </div>
-
-
-                            <div className="flex justify-around">
-                                
-
-                                <div className="hidden space-x-8 lg:-my-px lg:ms-10 lg:flex">
-                                    <NavLink className='text-purplePrimary'
-                                        // borderPage={currentRoute === '/' ? '1px solid black' : 'none'}
-                                        href="#" >
-                                           
-                                        Dashboard
-                                    </NavLink>
-
-                                    <NavLink
-                                        // borderPage={currentRoute === '/posts' ? '1px solid black' : 'none'}
-                                        href="#" to='/dashboard/postagens'>
-                                        Área de Postagens {/* fazer dropdown - resumo slide etc */}
-
-                                    </NavLink>
-                                    <NavLink
-                                        // borderPage={currentRoute === '/agenda' ? '1px solid black' : 'none'}
-                                        href="#" to='/dashboard/agenda' >
-                                        Agenda
-                                    </NavLink>
-                                    <NavLink
-                                        // borderPage={currentRoute === '/agenda' ? '1px solid black' : 'none'}
-                                        href="#" to='/dashboard/pomodoro' >
-                                        Método Pomodoro
-                                    </NavLink>
-                                    <NavLink href="#" to='/dashboard/notas'>Notas</NavLink>
-                                    
-                                </div>
-                                <div className='flex justify-between'>
-                               
-                                </div>
-                            </div>
-                            <div className='flex justify-center gap-16'>
-                            <NavLink
-                                        // borderPage={currentRoute === '/agenda' ? '1px solid black' : 'none'}
-                                        href="#" to='/dashboard/perfil' >
-                                        <svg xmlns="http://www.w3.org/2000/svg" height="14" width="12.25" viewBox="0 0 448 512">
-                                            <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/>
-                                        </svg>
-                                    </NavLink>
-                                    <LogoutButton />
-                            </div>
+                            <Link to="/">
+                                <Logo className="block h-12 w-auto fill-current" />
+                            </Link>
                         </div>
-               
+
+                        <div className="flex justify-around">
+                            <div className="space-x-8  lg:flex">
+                                <NavLink to="/dashboard" className="">
+                                    Dashboard
+                                </NavLink>
+                                <NavLink to="/dashboard/postagens" className="">
+                                    Área de Postagens
+                                </NavLink>
+                                <NavLink to="/dashboard/agenda" className="">
+                                    Agenda
+                                </NavLink>
+                                <NavLink to="/dashboard/pomodoro" className="">
+                                    Método Pomodoro
+                                </NavLink>
+                                <NavLink to="/dashboard/notas" className="">
+                                    Notas
+                                </NavLink>
+                            </div>
+                            <div className="flex justify-between"></div>
+                        </div>
+                        <div className="flex justify-center gap-16">
+                            <NavLink to="/dashboard/perfil" className="">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    height="14"
+                                    width="12.25"
+                                    viewBox="0 0 448 512"
+                                >
+                                    <path className='fill-gray-400' d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
+                                </svg>
+                            </NavLink>
+                            <LogoutButton />
+                        </div>
+                    </div>
                 </nav>
 
                 <UsuarioCard role={role} user={user} />
 
-                <div className='flex gap-8 mx-16'>
-                    <CardCategoria categoria={'Resumos'} customClass={'text-white'} />
-                    <CardCategoria categoria={'Apresentações'} customClass={'bg-white text-gray-800'} />
-                    <CardCategoria categoria={'Atividades'} customClass={'bg-white text-gray-800'} />
-
+                <div className="flex gap-8 mx-16">
+                    <CardCategoria
+                        categoria={'Resumos'}
+                        customClass={'text-white'}
+                    />
+                    <CardCategoria
+                        categoria={'Apresentações'}
+                        customClass={'bg-white text-gray-800'}
+                    />
+                    <CardCategoria
+                        categoria={'Atividades'}
+                        customClass={'bg-white text-gray-800'}
+                    />
                 </div>
 
-                <section className='mx-16 mt-16'>
-                    <h1 className='font-semibold text-2xl mb-8'>Tarefas Urgentes</h1>
+                <section className="mx-16 mt-16">
+                    <h1 className="font-semibold text-2xl mb-8">
+                        Tarefas Urgentes
+                    </h1>
 
-                    <div className='border-b border-gray-400 flex justify-between'>
-                        <div className='flex gap-8'>
+                    <div className="border-b border-gray-400 flex justify-between">
+                        <div className="flex gap-8">
                             <input type="checkbox" />
-                            <p className='text-lg'>Atividade de matemática sobre funções de segundo grau</p>
+                            <p className="text-lg">
+                                Atividade de matemática sobre funções de segundo
+                                grau
+                            </p>
                         </div>
 
                         <span>
@@ -106,11 +103,7 @@ export default function Dashboard() {
                         </span>
                     </div>
                 </section>
-
             </div>
-
         </>
-
-
     );
 }

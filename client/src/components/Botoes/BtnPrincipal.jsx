ff4855055a0be5e87247ba772e82fe1ed416dc71
unknown <sofiapparreira@gmail.com>
2024-07-29 11:46:42 -0300

    começo da programação do descanso no método pomodoro, e arrumei o tom de roxo

--- client/src/components/Botoes/BtnPrincipal.jsx
+++ client/src/components/Botoes/BtnPrincipal.jsx
@@ -6,9 +6,9 @@ export default function BtnPrincipal({ className = '', disabled, children, ...pr
             {...props}
             className={
                 `justify-center w-full items-center py-2
-                 bg-violet-800 hover:bg-violet-600 border border-transparent rounded-md 
+                 bg-purplePrimary hover:bg-purple-900 border border-transparent rounded-md 
                  font-medium text-white  
-                  hover:bg-gray-200 focus:bg-violet-700 active:bg-violet-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 ${disabled && 'opacity-25'
+                focus:bg-purplePrimary active:bg-purplePrimary focus:outline-none focus:ring-2 focus:ring-purplePrimary focus:ring-offset-2 transition ease-in-out duration-150 ${disabled && 'opacity-25'
                 } ` + className
             }
             disabled={disabled}

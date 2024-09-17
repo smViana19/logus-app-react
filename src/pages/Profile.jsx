1982f15fce15fca6fc2eb0460e7a9ee231fff585
Fernando Benicio <fernando.b.m2006@gmail.com>
2024-09-17 10:26:52 -0300

    navbar agenda, pomodoro, profile, area postagem + darkmode em ambas - não consegui passar o darkmode pro profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import NavLink from '../components/Navs/NavLink';
+import Navbar from '@/components/Navs/NavBar.jsx';
 import Logo from '../components/Logo/Logo.jsx';
 import { Link } from 'react-router-dom';
 import LogoutButton from '../components/Buttons/LogoutButton.jsx';
@@ -16,7 +17,7 @@ export default function Dashboard() {
     const user = useSelector(state => state.auth.user?.nome);
 
     if (!isLoggedIn) {
-       return <Navigate to="/login" replace />;
+        return <Navigate to="/login" replace />;
     }
 
     // Crie um estado para armazenar o ID do card aberto
@@ -27,49 +28,14 @@ export default function Dashboard() {
         setOpenCardId(openCardId === id ? null : id);
     }
 
+    
+
     return (
         <>
             <div className="min-h-screen bg-gray-50">
                 <nav className="bg-white border-b border-gray-50 shadow-md shadow-gray-50">
                     <MenuMobile />
-                    <div className="flex justify-between py-2 px-32">
-                        <div className="flex items-center">
-                            <Link to="/">
-                                <Logo className="block h-12 w-auto fill-current" />
-                            </Link>
-                        </div>
-
-                        <div className="flex justify-around">
-                            <div className="space-x-8 lg:flex">
-                                <NavLink to="/dashboard" className="">
-                                    Dashboard
-                                </NavLink>
-                                <NavLink to="/dashboard/postagens" className="">
-                                    Área de Postagens
-                                </NavLink>
-                                <NavLink to="/dashboard/agenda" className="">
-                                    Agenda
-                                </NavLink>
-                                <NavLink to="/dashboard/pomodoro" className="">
-                                    Método Pomodoro
-                                </NavLink>
-                              
-                            </div>
-                        </div>
-                        <div className="flex justify-center gap-16">
-                            <NavLink to="/dashboard/perfil" className="">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    height="14"
-                                    width="12.25"
-                                    viewBox="0 0 448 512"
-                                >
-                                    <path className='fill-gray-400' d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
-                                </svg>
-                            </NavLink>
-                            <LogoutButton />
-                        </div>
-                    </div>
+                    <Navbar />
                 </nav>
 
                 <div className='my-16 mx-32 '>

f1c00821c337f1ed39f48f73c64ec57e9eccaacf
Samuu <samuelfilipevianadois@gmail.com>
2024-09-27 20:50:53 -0300

    ajuste nas configuracoes do tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,19 +21,19 @@ export default {
     ],
     theme: {
         screens: {
-            'sm': '320px',
+            'sm': '640px',
             // => @media (min-width: 320px) { ... }
 
-            'md': '540px',
+            'md': '768px',
             // => @media (min-width: 540px) { ... }
 
-            'lg': '768px',
+            'lg': '1024px',
             // => @media (min-width: 768px) { ... }
 
-            'xl': '1024px',
+            'xl': '1280px',
             // => @media (min-width: 1024px) { ... }
 
-            '2xl': '1280px',
+            '2xl': '1536px',
             // => @media (min-width: 1280px) { ... }
         },
         extend: {

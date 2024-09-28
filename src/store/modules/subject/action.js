45fd61b3c77baeecd3b35b3fa90061f073430d16
Samuu <samuelfilipevianadois@gmail.com>
2024-09-28 20:17:18 -0300

    refatoração e melhoria
    - adicionado novas funcoes para requisicao do subject

--- src/store/modules/subject/action.js
+++ src/store/modules/subject/action.js
@@ -1,17 +1,23 @@
 import * as types from '../types';
-import axios from 'axios';
 
+
+export function subjectRequest(payload) {
+  return {
+    type: types.SUBJECT_REQUEST,
+    payload,
+  };
+}
 
 export const selectSubject = (subjectId) => {
-    return{
-        type: types.SELECT_SUBJECT,
-        payload: { subjectId },
-    }
-}
+  return {
+    type: types.SELECT_SUBJECT,
+    payload: { subjectId },
+  };
+};
 
 export const clearSubject = () => {
-    return{
-        type: types.CLEAR_SUBJECT,
-    }
-}
+  return {
+    type: types.CLEAR_SUBJECT,
+  };
+};
 

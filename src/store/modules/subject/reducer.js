076d69d53a83e9260fb1cd7f1179d06cbb00cc6a
Samuu <samuelfilipevianadois@gmail.com>
2024-09-28 20:14:08 -0300

    adicionado reducer com novas actions

--- src/store/modules/subject/reducer.js
+++ src/store/modules/subject/reducer.js
@@ -2,25 +2,49 @@ import * as types from '../types';
 
 
 const initialState = {
-    selectedSubjectId: null,
-}
+  selectedSubjectId: null,
+  isLoading: true,
+  subjects: [],
+  error: null,
+};
 
 const subjectReducer = (state = initialState, action) => {
-    switch (action.type){
-        case 'SELECT_SUBJECT':
-            return{
-                ...state,
-                selectedSubjectId: action.payload.subjectId
-            }
-
-        case 'CLEAR_SUBJECT':
-            return{
-                ...state,
-                selectedSubjectId: null,
-            }
-            default:
-                return state
+  switch (action.type) {
+    case types.SUBJECT_REQUEST: {
+      return {
+        ...state,
+        isLoading: true,
+        error: null,
+      };
     }
-}
+    case types.SUBJECT_SUCCESS: {
+      return {
+        ...state,
+        isLoading: false,
+        subjects: action.payload.subjects,
+      };
+    }
+    case types.SUBJECT_FAILURE: {
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload.error,
+      };
+    }
+    case types.SELECT_SUBJECT:
+      return {
+        ...state,
+        selectedSubjectId: action.payload.subjectId,
+      };
+
+    case types.CLEAR_SUBJECT:
+      return {
+        ...state,
+        selectedSubjectId: null,
+      };
+    default:
+      return state;
+  }
+};
 
 export default subjectReducer;

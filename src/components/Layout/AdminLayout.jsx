1e2156741da46ff718f98afd6180f3012c721c22
Samuu <samuelfilipevianadois@gmail.com>
2024-10-01 17:38:34 -0300

    alteracao nas rotas e começo da criacao da tela de admin
    - adicionado novo nome do arquivo para MainLayout
    - criado arquivos para as telas admin

---
+++ src/components/Layout/AdminLayout.jsx
@@ -0,0 +1,13 @@
+import AdminSidebar from '@/components/Sidebar/AdminSidebar.jsx';
+
+const AdminLayout = () => {
+  return (
+      <div>
+        <div>
+          <AdminSidebar />
+        </div>
+      </div>
+  );
+};
+
+export default AdminLayout;
---
+++ src/components/Sidebar/AdminSidebar.jsx
@@ -0,0 +1,7 @@
+const AdminSidebar = () => {
+  return(
+      <div>Admin Sidebar</div>
+  )
+};
+
+export default AdminSidebar;

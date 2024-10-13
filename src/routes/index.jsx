1262a4401910bdd3b2874dd141f6fbd5f903ac5d
Samuu <samuelfilipevianadois@gmail.com>
2024-10-13 16:49:15 -0300

    feat: inicio da nova tela de admin

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -25,29 +25,31 @@ export default function Rotas() {
 
   return (
 
-      <Routes> {/* Use o componente Routes para definir suas rotas */}
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/registro" element={<Registro />} />
+    <Routes> {/* Use o componente Routes para definir suas rotas */}
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/registro" element={<Registro />} />
 
-        <Route path="/dashboard" element={<MainLayout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="/dashboard/pomodoro" element={<Pomodoro />} />
-          <Route path="/dashboard/perfil" element={<Profile />} />
-          <Route path="/dashboard/postagens" element={<PostsArea />} />
-          <Route path="/dashboard/postagens/:nomeMateria" element={<Subject />} />
-          <Route path="/dashboard/postagens/:nomeMateria/:nomeAtiv" element={<AtividadeWrapper />} />
-          <Route path="/dashboard/agenda" element={<Schedule />} />
-          <Route path="/dashboard/settings" element={<Settings />} />
+      <Route path="/dashboard" element={<MainLayout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="/dashboard/pomodoro" element={<Pomodoro />} />
+        <Route path="/dashboard/perfil" element={<Profile />} />
+        <Route path="/dashboard/postagens" element={<PostsArea />} />
+        <Route path="/dashboard/postagens/:nomeMateria" element={<Subject />} />
+        <Route path="/dashboard/postagens/:nomeMateria/:nomeAtiv" element={<AtividadeWrapper />} />
+        <Route path="/dashboard/agenda" element={<Schedule />} />
+        <Route path="/dashboard/settings" element={<Settings />} />
 
-        </Route>
-
+      </Route>
+      <Route path='/admin' element={<MainLayout />}>
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
         <Route path="/admin/notas/grade" element={<AdminSchool />} />
         <Route path="/admin/notas" element={<AdminSchoolGrade />} />
+      </Route>
 
-      </Routes>
+
+    </Routes>
 
 
   );
@@ -56,8 +58,8 @@ export default function Rotas() {
 
 const AtividadeWrapper = () => {
   return (
-      <AtividadeProvider>
-        <SendTaskSubject />
-      </AtividadeProvider>
+    <AtividadeProvider>
+      <SendTaskSubject />
+    </AtividadeProvider>
   );
 };

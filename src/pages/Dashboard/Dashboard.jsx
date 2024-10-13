50d7d99e04ec22a8f65a00f9fa7d64fea2fa84b2
Samuu <samuelfilipevianadois@gmail.com>
2024-10-13 19:47:28 -0300

    refactor: retirado componente temporario

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import ProjectStatics from '@/components/ProjectsStatistics/ProjectStatics.jsx';
 import Plataform from '@/components/Plataform/Plataform.jsx';
-import ProjectCard from '@/components/CardsContainers/ProjectCard.jsx';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
@@ -20,31 +19,26 @@ export default function Dashboard() {
     { name: 'Teste 4', type: 'Teste 4', date: '27-10-2024', members: ['Joao', 'Maria', 'Jose'], files: 4, progress: 20, },
   ];
 
-
   useEffect(() => {
-    if(user && user.role === "diretor"){
+    if (user && user.role === "diretor") {
       navigate("/admin/dashboard")
-    } 
+    }
   }, [user, navigate])
   return (
-      <div className="p-5 min-h-screen sm:ml-20 lg:ml-64 mt-24 ml-14 md:ml-64 transition-all duration-300">
-        
-        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 md:grid-cols-1 gap-4">
-          <ProjectStatics />
-          <Plataform />
-          <ProjectStatics />
-        </div>
+    <div className="p-5 min-h-screen sm:ml-20 lg:ml-64 mt-24 ml-14 md:ml-64 transition-all duration-300">
 
-        <div>
-          <div className="flex justify-between items-center py-4 mt-5">
-            <h1 className="text-lg font-semibold dark:text-white ml-2">Útlimas atividades</h1>
-          </div>
-
-          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4">
-            {projects && projects.map((project, index) => <ProjectCard key={index} project={project} />)}
-          </div>
-        </div>
-        
+      <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 md:grid-cols-1 gap-4">
+        <ProjectStatics />
+        <Plataform />
+        <ProjectStatics />
       </div>
+
+      <div>
+        <div className="flex justify-between items-center py-4 mt-5">
+          <h1 className="text-lg font-semibold dark:text-white ml-2">Útlimas atividades</h1>
+        </div>
+      </div>
+
+    </div>
   );
 }

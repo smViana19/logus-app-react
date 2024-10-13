a05718f58bd47a496322331cae8e1e1555ac19aa
Samuu <samuelfilipevianadois@gmail.com>
2024-10-13 19:51:41 -0300

    feat: adicionado novas paginas para diretor

---
+++ src/pages/Admin/AdminReport/AdminReport.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminReport = () => {
+    return (
+        <div>
+            AdminReport
+        </div>
+    );
+}
+
+export default AdminReport;
---
+++ src/pages/Admin/AdminStudents/AdminStudents.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminStudents = () => {
+    return (
+        <div>
+            AdminStudents
+        </div>
+    );
+}
+
+export default AdminStudents;

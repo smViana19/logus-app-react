47bc05335d0776c4ec1aa1d5b1d1e6b33f3f432c
sofia <sofiapparreira@gmail.com>
2024-10-14 16:58:57 -0300

    sound sea and library

--- src/components/Modal/ModalPomodoroSound.jsx
+++ src/components/Modal/ModalPomodoroSound.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Howl, Howler } from 'howler';
 import rainSound from '../../assets/audios/rainSound.mp3';
+import seaSound from '../../assets/audios/mar.mp3';
+import librarySound from '../../assets/audios/biblioteca.mp3';
 
 export default function ModalPomodoroSound({ isOpen, setOpenModal }) {
   const handleClose = (e) => {
@@ -11,7 +13,7 @@ export default function ModalPomodoroSound({ isOpen, setOpenModal }) {
 
   const [selectedSound, setSelectedSound] = useState('Nenhum');
   const [soundInstance, setSoundInstance] = useState(null);
-  const [volume, setVolume] = useState(1); 
+  const [volume, setVolume] = useState(1);
 
   const playSound = (soundFile, soundName) => {
     if (soundInstance) {
@@ -28,8 +30,8 @@ export default function ModalPomodoroSound({ isOpen, setOpenModal }) {
     const sound = new Howl({
       src: [soundFile],
       html5: true,
-      volume: volume, 
-      onplay: () => setSoundInstance(sound), 
+      volume: volume,
+      onplay: () => setSoundInstance(sound),
       onend: () => {
         setSoundInstance(null);
         setSelectedSound('Nenhum');
@@ -42,7 +44,48 @@ export default function ModalPomodoroSound({ isOpen, setOpenModal }) {
 
   const sounds = {
     Rain: rainSound,
-    Study: '/path/to/study-sound.mp3', 
+    Sea: seaSound,
+    Library: librarySound,
+  };
+
+  const soundIcons = {
+    Rain: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="20"
+        width="20"
+        viewBox="0 0 512 512"
+      >
+        <path
+          className="fill-purplePrimary"
+          d="M0 224c0 53 43 96 96 96l47.2 0L290 202.5c17.6-14.1 42.6-14 60.2 .2s22.8 38.6 12.8 58.8L333.7 320l18.3 0 64 0c53 0 96-43 96-96s-43-96-96-96c-.5 0-1.1 0-1.6 0c1.1-5.2 1.6-10.5 1.6-16c0-44.2-35.8-80-80-80c-24.3 0-46.1 10.9-60.8 28C256.5 24.3 219.1 0 176 0C114.1 0 64 50.1 64 112c0 7.1 .7 14.1 1.9 20.8C27.6 145.4 0 181.5 0 224zm330.1 3.6c-5.8-4.7-14.2-4.7-20.1-.1l-160 128c-5.3 4.2-7.4 11.4-5.1 17.8s8.3 10.7 15.1 10.7l70.1 0L177.7 488.8c-3.4 6.7-1.6 14.9 4.3 19.6s14.2 4.7 20.1 .1l160-128c5.3-4.2 7.4-11.4 5.1-17.8s-8.3-10.7-15.1-10.7l-70.1 0 52.4-104.8c3.4-6.7 1.6-14.9-4.2-19.6z"
+        />
+      </svg>
+    ),
+    Sea: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="19"
+        width="20.75"
+        viewBox="0 0 576 512"
+      >
+        <path
+          className="fill-purplePrimary"
+          d="M269.5 69.9c11.1-7.9 25.9-7.9 37 0C329 85.4 356.5 96 384 96c26.9 0 55.4-10.8 77.4-26.1c0 0 0 0 0 0c11.9-8.5 28.1-7.8 39.2 1.7c14.4 11.9 32.5 21 50.6 25.2c17.2 4 27.9 21.2 23.9 38.4s-21.2 27.9-38.4 23.9c-24.5-5.7-44.9-16.5-58.2-25C449.5 149.7 417 160 384 160c-31.9 0-60.6-9.9-80.4-18.9c-5.8-2.7-11.1-5.3-15.6-7.7c-4.5 2.4-9.7 5.1-15.6 7.7c-19.8 9-48.5 18.9-80.4 18.9c-33 0-65.5-10.3-94.5-25.8c-13.4 8.4-33.7 19.3-58.2 25c-17.2 4-34.4-6.7-38.4-23.9s6.7-34.4 23.9-38.4C42.8 92.6 61 83.5 75.3 71.6c11.1-9.5 27.3-10.1 39.2-1.7c0 0 0 0 0 0C136.7 85.2 165.1 96 192 96c27.5 0 55-10.6 77.5-26.1zm37 288C329 373.4 356.5 384 384 384c26.9 0 55.4-10.8 77.4-26.1c0 0 0 0 0 0c11.9-8.5 28.1-7.8 39.2 1.7c14.4 11.9 32.5 21 50.6 25.2c17.2 4 27.9 21.2 23.9 38.4s-21.2 27.9-38.4 23.9c-24.5-5.7-44.9-16.5-58.2-25C449.5 437.7 417 448 384 448c-31.9 0-60.6-9.9-80.4-18.9c-5.8-2.7-11.1-5.3-15.6-7.7c-4.5 2.4-9.7 5.1-15.6 7.7c-19.8 9-48.5 18.9-80.4 18.9c-33 0-65.5-10.3-94.5-25.8c-13.4 8.4-33.7 19.3-58.2 25c-17.2 4-34.4-6.7-38.4-23.9s6.7-34.4 23.9-38.4c18.1-4.2 36.2-13.3 50.6-25.2c11.1-9.4 27.3-10.1 39.2-1.7c0 0 0 0 0 0C136.7 373.2 165.1 384 192 384c27.5 0 55-10.6 77.5-26.1c11.1-7.9 25.9-7.9 37 0zm0-144C329 229.4 356.5 240 384 240c26.9 0 55.4-10.8 77.4-26.1c0 0 0 0 0 0c11.9-8.5 28.1-7.8 39.2 1.7c14.4 11.9 32.5 21 50.6 25.2c17.2 4 27.9 21.2 23.9 38.4s-21.2 27.9-38.4 23.9c-24.5-5.7-44.9-16.5-58.2-25C449.5 293.7 417 304 384 304c-31.9 0-60.6-9.9-80.4-18.9c-5.8-2.7-11.1-5.3-15.6-7.7c-4.5 2.4-9.7 5.1-15.6 7.7c-19.8 9-48.5 18.9-80.4 18.9c-33 0-65.5-10.3-94.5-25.8c-13.4 8.4-33.7 19.3-58.2 25c-17.2 4-34.4-6.7-38.4-23.9s6.7-34.4 23.9-38.4c18.1-4.2 36.2-13.3 50.6-25.2c11.1-9.5 27.3-10.1 39.2-1.7c0 0 0 0 0 0C136.7 229.2 165.1 240 192 240c27.5 0 55-10.6 77.5-26.1c11.1-7.9 25.9-7.9 37 0z"
+        />
+      </svg>
+    ),
+    Library: (
+      <svg
+        className="fill-purplePrimary"
+        xmlns="http://www.w3.org/2000/svg"
+        height="19"
+        width="20.75"
+        viewBox="0 0 576 512"
+      >
+        <path d="M249.6 471.5c10.8 3.8 22.4-4.1 22.4-15.5l0-377.4c0-4.2-1.6-8.4-5-11C247.4 52 202.4 32 144 32C93.5 32 46.3 45.3 18.1 56.1C6.8 60.5 0 71.7 0 83.8L0 454.1c0 11.9 12.8 20.2 24.1 16.5C55.6 460.1 105.5 448 144 448c33.9 0 79 14 105.6 23.5zm76.8 0C353 462 398.1 448 432 448c38.5 0 88.4 12.1 119.9 22.6c11.3 3.8 24.1-4.6 24.1-16.5l0-370.3c0-12.1-6.8-23.3-18.1-27.6C529.7 45.3 482.5 32 432 32c-58.4 0-103.4 20-123 35.6c-3.3 2.6-5 6.8-5 11L304 456c0 11.4 11.7 19.3 22.4 15.5z" />
+      </svg>
+    ),
   };
 
   if (!isOpen) return null;
@@ -66,19 +109,9 @@ export default function ModalPomodoroSound({ isOpen, setOpenModal }) {
                 setSelectedSound(sound);
                 playSound(sounds[sound], sound);
               }}
-              className="flex px-8 gap-8 items-center py-3 border-b border-gray-200 dark:border-zinc-700 hover:bg-gray-100 dark:hover:bg-zinc-700 cursor-pointer "
+              className="flex px-8 gap-8 items-center py-3 border-b border-gray-200 dark:border-zinc-700 hover:bg-gray-100 dark:hover:bg-zinc-700 cursor-pointer"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="20"
-                width="20"
-                viewBox="0 0 512 512"
-              >
-                <path
-                  className="fill-purplePrimary"
-                  d="M0 224c0 53 43 96 96 96l47.2 0L290 202.5c17.6-14.1 42.6-14 60.2 .2s22.8 38.6 12.8 58.8L333.7 320l18.3 0 64 0c53 0 96-43 96-96s-43-96-96-96c-.5 0-1.1 0-1.6 0c1.1-5.2 1.6-10.5 1.6-16c0-44.2-35.8-80-80-80c-24.3 0-46.1 10.9-60.8 28C256.5 24.3 219.1 0 176 0C114.1 0 64 50.1 64 112c0 7.1 .7 14.1 1.9 20.8C27.6 145.4 0 181.5 0 224zm330.1 3.6c-5.8-4.7-14.2-4.7-20.1-.1l-160 128c-5.3 4.2-7.4 11.4-5.1 17.8s8.3 10.7 15.1 10.7l70.1 0L177.7 488.8c-3.4 6.7-1.6 14.9 4.3 19.6s14.2 4.7 20.1 .1l160-128c5.3-4.2 7.4-11.4 5.1-17.8s-8.3-10.7-15.1-10.7l-70.1 0 52.4-104.8c3.4-6.7 1.6-14.9-4.2-19.6z"
-                />
-              </svg>
+              {soundIcons[sound]}
               <span className="text-gray-700 font-medium dark:text-white">
                 {sound}
               </span>
@@ -124,12 +157,12 @@ export default function ModalPomodoroSound({ isOpen, setOpenModal }) {
               input[type='range']::-webkit-slider-thumb {
                 -webkit-appearance: none;
                 appearance: none;
-                width: 14px; /* Tamanho da bolinha */
-                height: 14px; /* Tamanho da bolinha */
-                border-radius: 100%; /* Bordas arredondadas */
-                background: #820ad1; /* Cor da bolinha */
-                cursor: pointer; /* Cursor de ponteiro ao passar por cima */
-                transition: background 0.3s; /* Efeito de transição ao passar por cima */
+                width: 14px;
+                height: 14px;
+                border-radius: 100%;
+                background: #820ad1;
+                cursor: pointer;
+                transition: background 0.3s;
               }
             `}</style>
           </div>

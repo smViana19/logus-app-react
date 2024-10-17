7333f1ccae92d0d79860f1a26b534ba9e875aaf1
sofia <sofiapparreira@gmail.com>
2024-10-17 08:03:56 -0300

    consertando o header da landing page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -28,23 +28,10 @@ export default function LandingPage() {
           <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
             <a href="#" class="flex items-center">
               <img src={logo} class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-              <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-                Landwind
-              </span>
+             
             </a>
             <div class="flex items-center lg:order-2">
-              <div class="hidden mt-2 mr-4 sm:inline-block">
-                <a
-                  class="github-button"
-                  href="https://github.com/themesberg/landwind"
-                  data-size="large"
-                  data-icon="octicon-star"
-                  data-show-count="true"
-                  aria-label="Star themesberg/landwind on GitHub"
-                >
-                  Star
-                </a>
-              </div>
+              
 
               <div>
                 {isLoggedIn ? (

c8e89d20f1ca56eb14faec8c4513b09c33f77968
Samuu <samuelfilipevianadois@gmail.com>
2024-10-28 19:58:32 -0300

    refactor: changes axios instance - removed deprecated axios instance - addes logical token jwt

--- services/axios.js
+++ services/axios.js
@@ -1,5 +1,19 @@
 import axios from 'axios';
+import store from '../src/store';
 
-export default axios.create({
+const axiosInstance = axios.create({
     baseURL: 'http://localhost:3000',
 });
+
+axiosInstance.interceptors.request.use((config) => {
+    const state = store.getState();
+    const token = state.auth.token;
+
+    console.log('Token:', token);
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, (error) => Promise.reject(error))
+
+export default axiosInstance;
